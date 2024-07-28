1a13b69197ab26a6b68f0cb478b6c7f7537efa0c
Shanta85 <rshanta361@gmail.com>
2024-07-28 10:04:34 +0530

    Initial commit

---
+++ client/3d picture/pyt.py
@@ -0,0 +1,57 @@
+from  flet import*
+def main(page: Page):
+    BG='#041955'
+    FWG='#97b4ff'
+    FG='#3450a1'
+    pink='#eb06ff'
+    first_page_contents=Container(
+        content=Column(
+            controls=[
+                Row(
+                    controls=[
+                        Container(
+                            content=Icon(icons.MENU)
+                        ),
+                        Row(
+                            controls=[
+                                Icon(icons.SEARCH)
+                            ]
+                        )
+                    ]
+                )
+            ]
+        )
+    )
+    page_1=Container()
+    page_2=Row(
+        controls=[
+           Container(
+               width=400,
+               height=880,
+               bgcolor=FG,
+               border_radius=35,
+               padding=padding.only(
+                   top=50,left=20,right=20,bottom=5
+               ),
+               content=Column(
+                   controls=[
+                       first_page_contents
+                   ]
+               )
+           ) 
+        ]
+    )
+    
+    container=Container(width=400,
+                        height=880
+                        ,bgcolor=BG,
+                        border_radius=35,
+                        content=Stack(
+                            controls=[
+                                page_1,
+                                page_2,
+                            ]
+                        ))
+        
+    page.add(container)
+app(target=main)
